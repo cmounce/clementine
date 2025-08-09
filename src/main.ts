128800eda8f9ecf5847856d3063652dbaa269e97
Chris Mounce <christophermounce@gmail.com>
2025-08-09 14:46:59 -0700

    Initial project creation

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>Story Editor</h1>
+  <p>Your TypeScript + Vite app is ready!</p>
+`;
